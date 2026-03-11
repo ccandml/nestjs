65c5f417a4c9bd929e3bab2097d79627422031eb
陈泳熙 <16270908+chen-yongxi666@user.noreply.gitee.com>
2026-03-11 16:28:22 +0800

    修改ormconfig-->root目录，删除app.module中的配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { RangeModule } from './range/range.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { ConfigEnum } from './enum/config.enum';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import * as Joi from 'joi';
-import { User } from './user/user.entity';
-import { Profile } from './user/profile.entity';
-import { Logs } from './logs/logs.entity';
-import { Roles } from './roles/roles.entity';
 import { RolesModule } from './roles/roles.module';
 import { LogsModule } from './logs/logs.module';
+import ormconfig from 'ormconfig';
 
 const envFilePath = `.env.${process.env.NODE_ENV || `development`}`;
 
@@ -31,25 +27,7 @@ const envFilePath = `.env.${process.env.NODE_ENV || `development`}`;
       }),
     }),
     // 连接数据库的装修队
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService], //给装修队介绍管家
-      // 装修队要问管家拿到对应的配置来连接数据库
-      useFactory: (configService: ConfigService) =>
-        ({
-          // 管家configService 靠get()拿到配置
-          type: configService.get(ConfigEnum.DB_TYPE),
-          host: configService.get(ConfigEnum.DB_HOST),
-          port: configService.get(ConfigEnum.DB_PORT),
-          username: configService.get(ConfigEnum.DB_USERNAME),
-          password: configService.get(ConfigEnum.DB_PASSWORD),
-          database: configService.get(ConfigEnum.DB_DATABASE),
-          entities: [User, Profile, Logs, Roles],
-          synchronize: configService.get(ConfigEnum.DB_SYNCHRONIZE),
-          // logging: ['error'],
-          logging: process.env.NODE_ENV === 'development', // 开发环境下打印sql语句
-        }) as TypeOrmModuleOptions,
-    }),
+    TypeOrmModule.forRoot(ormconfig),
     UserModule,
     RangeModule,
     RolesModule,
